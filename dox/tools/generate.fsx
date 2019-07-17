910de7865534162a98a50a9b8e918ef037c31ff6
Daniel J. Summers <daniel.summers.2415@gmail.com>
2019-07-16 22:34:03 -0500

    Generate script works
    Still getting errors about needing to reference netstandard.dll, but this only keeps the tooltips from being generated for statements; it needs to be fixed, but it'll be in the next round.

--- dox/tools/generate.fsx
+++ dox/tools/generate.fsx
@@ -3,13 +3,6 @@
 // (the generated documentation is stored in the 'docs/output' directory)
 // --------------------------------------------------------------------------------------
 
-// Binaries that have XML documentation (in a corresponding generated XML file)
-// Any binary output / copied to bin/projectName/projectName.dll will
-// automatically be added as a binary to generate API docs for.
-// for binaries output to root bin folder please add the filename only to the 
-// referenceBinaries list below in order to generate documentation for the binaries.
-// (This is the original behaviour of ProjectScaffold prior to multi project support)
-let referenceBinaries = []
 // Web site location for the generated documentation
 let website = "/FromObjectsToFunctions"
 
@@ -32,9 +25,7 @@ let info =
 #r "FakeLib.dll"
 open Fake
 open System.IO
-open Fake.FileHelper
-open FSharp.Literate
-open FSharp.MetadataFormat
+open FSharp.Formatting.Razor
 
 // When called from 'build.fsx', use the public project URL as <root>
 // otherwise, use the current 'output' directory.
@@ -54,92 +45,54 @@ let formatting = __SOURCE_DIRECTORY__ @@ "../../packages/FSharp.Formatting/"
 let docTemplate = "docpage.cshtml"
 
 // Where to look for *.csproj templates (in this order)
-let layoutRootsAll = new System.Collections.Generic.Dictionary<string, string list>()
-layoutRootsAll.Add("en",[ templates; formatting @@ "templates"
-                          formatting @@ "templates/reference" ])
+let layoutRootsAll = System.Collections.Generic.Dictionary<string, string list>()
+layoutRootsAll.Add ("en",[ templates; formatting @@ "templates"; formatting @@ "templates/reference" ])
 subDirectories (directoryInfo templates)
 |> Seq.iter (fun d ->
-                let name = d.Name
-                if name.Length = 2 || name.Length = 3 then
-                    layoutRootsAll.Add(
-                            name, [templates @@ name
-                                   formatting @@ "templates"
-                                   formatting @@ "templates/reference" ]))
+    match d.Name.Length with
+    | 2 | 3 ->
+      layoutRootsAll.Add (
+        d.Name,
+        [templates @@ d.Name; formatting @@ "templates"; formatting @@ "templates/reference" ])
+    | _ -> ())
 
 // Copy static files and CSS + JS from F# Formatting
 let copyFiles () =
-  CopyRecursive files output true |> Log "Copying file: "
+  CopyRecursive files output true
+  |> Log "Copying file: "
   ensureDirectory (output @@ "content")
   CopyRecursive (formatting @@ "styles") (output @@ "content") true 
-    |> Log "Copying styles and scripts: "
-
-let binaries =
-    let manuallyAdded = 
-        referenceBinaries 
-        |> List.map (fun b -> bin @@ b)
-    
-    let conventionBased = []
-        (*directoryInfo bin 
-        |> subDirectories
-        |> Array.map (fun d -> d.FullName @@ (sprintf "%s.dll" d.Name))
-        |> List.ofArray *)
-
-    conventionBased @ manuallyAdded
-
-let libDirs = []
-    (*let conventionBasedbinDirs =
-        directoryInfo bin 
-        |> subDirectories
-        |> Array.map (fun d -> d.FullName)
-        |> List.ofArray
-
-    conventionBasedbinDirs @ [bin]*)
-
-// Build API reference from XML comments
-let buildReference () =
-  CleanDir (output @@ "reference")
-  MetadataFormat.Generate
-    ( binaries, output @@ "reference", layoutRootsAll.["en"],
-      parameters = ("root", root)::info,
-      sourceRepo = githubLink @@ "tree/master",
-      sourceFolder = __SOURCE_DIRECTORY__ @@ ".." @@ "..",
-      publicOnly = true,libDirs = libDirs )
+  |> Log "Copying styles and scripts: "
 
 // Build documentation from `fsx` and `md` files in `docs/content`
 let buildDocumentation () =
 
   // First, process files which are placed in the content root directory.
-
-  Literate.ProcessDirectory
-    ( content, docTemplate, output, replacements = ("root", root)::info,
-      layoutRoots = layoutRootsAll.["en"],
-      generateAnchors = true,
+  RazorLiterate.ProcessDirectory
+    ( content, docTemplate, output,
+      layoutRoots      = layoutRootsAll.["en"],
+      replacements     = ("root", root)::info,
+      generateAnchors  = true,
       processRecursive = false)
 
   // And then process files which are placed in the sub directories
   // (some sub directories might be for specific language).
-
   let subdirs = Directory.EnumerateDirectories(content, "*", SearchOption.TopDirectoryOnly)
   for dir in subdirs do
     let dirname = (new DirectoryInfo(dir)).Name
     let layoutRoots =
-        // Check whether this directory name is for specific language
-        let key = layoutRootsAll.Keys
-                  |> Seq.tryFind (fun i -> i = dirname)
-        match key with
-        | Some lang -> layoutRootsAll.[lang]
-        | None -> layoutRootsAll.["en"] // "en" is the default language
-
-    Literate.ProcessDirectory
-      ( dir, docTemplate, output @@ dirname, replacements = ("root", root)::info,
-        layoutRoots = layoutRoots,
+      // Check whether this directory name is for specific language
+      let key = layoutRootsAll.Keys
+                |> Seq.tryFind (fun i -> i = dirname)
+      match key with
+      | Some lang -> layoutRootsAll.[lang]
+      | None -> layoutRootsAll.["en"] // "en" is the default language
+    RazorLiterate.ProcessDirectory
+      ( dir, docTemplate, output @@ dirname,
+        layoutRoots     = layoutRoots,
+        replacements    = ("root", root) :: info,
         generateAnchors = true )
 
 // Generate
 copyFiles()
-//#if HELP
 buildDocumentation()
-//#endif
-//#if REFERENCE
-//buildReference()
-//#endif
